07e010d1a9585698ebc0a43a2f748cf6d6eac358
gary347 <54109863+gary347@users.noreply.github.com>
2021-05-20 19:00:27 +0530

    main.j

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-var GuestName = document.getElementById("Name11").value;
+var GuestName ;
 var names_of_people = [];
 
 function add()
 {
 var GuestName = document.getElementById("Name11").value;
-names_of_people.push(GuestName+",");
-document.getElementById("Name1").value;
+names_of_people.push(GuestName);
+document.getElementById("Name1").value = names_of_people;
 }
 
 function show()
